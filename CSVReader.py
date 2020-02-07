3e455029437b20cdb466d4c62fa14636e6db3b76
Jan Domingo <janaarondomingo@gmail.com>
2020-02-06 16:03:09 -0800

    Formatted csv output, included sample rocket data

--- CSVReader.py
+++ CSVReader.py
@@ -1,7 +1,9 @@
 import csv
+import time
 
-with open('test_values.txt', 'r') as csv_file:
+with open('example.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for line in csv_reader:
-        print(line)
+        print('{:<15} {:15} {:<20} {:25}'.format(*line))
+        time.sleep(0.1)
